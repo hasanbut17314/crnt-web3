032eb7600a0bc633783c39024cb96d37b7b98478
almovidhussaini <shahalmovid@gmail.com>
2024-12-26 21:08:50 +0500

    added view and download links

--- src/components/WhitePaper/WhitePaper.jsx
+++ src/components/WhitePaper/WhitePaper.jsx
@@ -49,7 +49,7 @@ const WhitePaper = () => {
                   <a
                     className="btn btn-two"
                     href={
-                      "https://drive.google.com/file/d/1lfBxPUy-3-bobEDkt7qVyF9yrCRp2v-W/view?usp=sharing"
+                      "https://drive.google.com/file/d/1Eh5_iC_SWjbplpd7GyP1a8-_M1hu5X0E/view?usp=sharing"
                     }
                     target="_blank"
                   >
@@ -73,7 +73,7 @@ const WhitePaper = () => {
                   />
                 </Modal> */}
                   <a
-                    href={`https://drive.usercontent.google.com/u/0/uc?id=1lfBxPUy-3-bobEDkt7qVyF9yrCRp2v-W&export=download`}
+                    href={`https://drive.usercontent.google.com/u/0/uc?id=1Eh5_iC_SWjbplpd7GyP1a8-_M1hu5X0E&export=download`}
                     download={"CRNT Whitepaper"}
                     className="btn btn-two"
                     style={{ marginLeft: "5px" }}
