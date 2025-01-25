ffa20eef879cab5586800fbe7db941556119b846
almovidhussaini <shahalmovid@gmail.com>
2025-01-25 14:28:54 +0500

    added development

--- src/App.js
+++ src/App.js
@@ -30,25 +30,25 @@ const projectId = ProjectId;
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 
 
-// const wagmiConfig = createConfig({
-//   autoConnect: true,
-//   connectors: w3mConnectors({ version: 1, chains, projectId }),
-//   publicClient,
-  
-// });
-
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ version: 1, chains, projectId }),
   publicClient,
-  publicProvider: {
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Allows all origins
-      'Access-Control-Allow-Headers': '*', // Allows all headers
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Allowed methods
-    },
-  },
+  
 });
+
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors: w3mConnectors({ version: 1, chains, projectId }),
+//   publicClient,
+//   publicProvider: {
+//     headers: {
+//       'Access-Control-Allow-Origin': '*', // Allows all origins
+//       'Access-Control-Allow-Headers': '*', // Allows all headers
+//       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Allowed methods
+//     },
+//   },
+// });
 
 // 3. Configure modal ethereum client
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
